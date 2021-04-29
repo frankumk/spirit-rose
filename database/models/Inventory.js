ef02fc97d053c562cfddc8224a0e59bfcea4d4ca
Kayla <frankumk@gmail.com>
2021-04-28 21:12:26 -0700

    one more time

---
+++ database/models/Inventory.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize')
+const { STRING, INTEGER, FLOAT } = Sequelize
+const db = require('../db')
+
+const Inventory = db.define('inventory',{
+  item: {
+    type: STRING,
+    allowNull: false
+  },
+  price: {
+    type: FLOAT,
+  },
+  description: {
+    type: STRING,
+  },
+  qty: {
+    type: INTEGER,
+  }
+})
+
+module.exports = Inventory
