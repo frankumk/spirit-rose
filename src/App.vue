c88afb2549ca1f9fcccb2beaa9da01e2fbaf8d0b
Kayla <frankumk@gmail.com>
2021-06-25 13:26:05 -0700

    Changed nav a bit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <template>
   <v-app>
-    <!-- <div id="app">
-      <v-toolbar app color="#CC5500">
-        <div id="nav">
-          <router-link to="/">Home</router-link>        
-          <router-link to="/roses">Rose Medicine</router-link>       
-          <router-link to="/rituals">Ritual Creation</router-link>       
-          <router-link to="/yonisteam">Yoni Steaming</router-link>      
-          <router-link to="/herbs">More Herbs</router-link>      
-          <router-link to="/wombhealing">Womb Healing</router-link>
-        </div>
-      </v-toolbar>
-    <router-view/> -->
 <div id="nav">
-  <v-toolbar color="#CC5500" dark>
-    <!-- <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon> -->
+  <!-- <v-toolbar color="#CC5500" dark>
     <v-tabs v-model="tab"> 
       <v-tab left to="/">Spirit Rose</v-tab>
       <v-spacer></v-spacer>
       <v-tab right grow to="/roses">Rose Medicine</v-tab>
-      <v-tab right grow to="/rituals">Ritual Cretion</v-tab>
+      <v-tab right grow to="/rituals">Ritual Creation</v-tab>
       <v-tab right grow to="/yonisteam">Yoni Steam</v-tab>
       <v-tab right grow to="/herbs">Herbs</v-tab>
       <v-tab right grow to="/wombhealing">Womb Healing</v-tab>
@@ -29,12 +16,66 @@
       mdi-cart
     </v-icon>
     
-  </v-toolbar>
+  </v-toolbar> -->
+
+    <v-app-bar
+      absolute
+      color="#CC5500"
+      dark
+      height="64px"
+    >
+      <v-menu
+        bottom
+        right
+        class="hidden-sm-and-up"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            dark
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-app-bar-nav-icon class="hidden-sm-and-up"></v-app-bar-nav-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="header in headers"
+            :key="header"
+          >
+            <v-list-item-title>{{ header.name }}</v-list-item-title>
+            <v-divider></v-divider>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-toolbar-title class="text-no-wrap">Spirit Rose</v-toolbar-title>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title class="hidden-xs-only">
+          <v-tab exact v-for="header in headers" :key="header.id" :to="header.path">{{ header.name }}</v-tab>
+        </v-tabs>
+
+        <v-spacer></v-spacer>
+
+
+        <v-btn
+          icon
+        >
+          <v-icon>mdi-cart</v-icon>
+        </v-btn>
+      </template>
+
+    </v-app-bar>
   </div>
-    
+
+
     <v-main>
-      <router-view></router-view>
+      <router-view class="mt-16 pt-16"></router-view>
     </v-main>
+
     <v-footer color="#CC5500">
       <v-col
         class="text-center"
@@ -58,6 +99,13 @@ export default{
   data: () => ({
     drawer: false,
     tab: null,
+    headers: [
+      { id: 0, name: "Home", path: "/" },
+      { id: 1, name: "Medicine", path: "/herbs" },
+      { id: 2, name: "Healing", path: "/wombhealing" },
+      { id: 3, name: "Rituals", path: "/rituals" },
+      { id: 4, name: "Sale", path: "/roses" }
+    ],
     icons: [
       'mdi-calendar',
       'mdi-instagram',
